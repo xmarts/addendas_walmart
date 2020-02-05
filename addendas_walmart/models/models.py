5461e102878e835796628436d26097a57cd7d4cd
William Colin <williamcolin954@gmail.com>
2020-02-05 16:26:14 -0600

    *Se agregan los saltos de linea para cada letra H,C,S.. *Se agregan saltos de linea y etiquetas para que reconozcan el mismo salto en notepad++ * Se corrige el campo de numero de cuenta bancaria

--- addendas_walmart/models/models.py
+++ addendas_walmart/models/models.py
@@ -2,7 +2,3 @@
 
 from odoo import models, fields, api
 
-# class AddFieldsAdendaWalmart(models.Model):
-#     _inherit = 'res.partner'
-
-#     addres_contact_name = fields.Char(string="Child Contact Name")
