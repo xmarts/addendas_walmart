9c161a2d90dfe386f7ab469ccd46c88969572cff
javierhilarioxmarts <javier.hilario.xmarts@gmail.com>
2022-04-21 16:34:25 -0500

    [REF] Migrate to version 15.0

--- addendas_walmart/__manifest__.py
+++ addendas_walmart/__manifest__.py
@@ -1,35 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': "addendas_walmart",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
+    'summary': """Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
-    # for the full list
+    'description': """Long description of module's purpose""",
+    'author': "Xmarts",
+    'contributors': "victoralonso@xmarts.com, javier.hilario@xmarts.com",
+    'website': "http://www.xmarts.com",
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'version': '15.0.1.0.0',
+    'depends': ['base', 'l10n_mx_edi', 'fields_functions_jarochito'],
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/addenda_walmart.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
