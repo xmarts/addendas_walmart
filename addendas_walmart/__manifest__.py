ed6daa89c2199dc8ae9133be189b98ecaa6e6fe4
jose <jose.vences.xmarts@gmail.com>
2023-06-21 09:41:16 -0600

    [FIX] Update

--- addendas_walmart/__manifest__.py
+++ addendas_walmart/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "http://www.xmarts.com",
     'category': 'Uncategorized',
     'version': '15.0.1.0.0',
-    'depends': ['base', 'l10n_mx_edi', 'fields_functions_adendas'],
+    'depends': ['base', 'l10n_mx_edi', 'fields_functions_adendas', 'l10n_mx_edi_40'],
     'data': [
         # 'security/ir.model.access.csv',
         'views/addenda_walmart.xml',
