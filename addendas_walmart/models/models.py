d227b08559e052b6a7e0b7a9e4d914f8af8a3672
David <david.diaz.xmarts@gmail.com>
2023-06-28 13:13:01 -0600

    [UPD] Test method to get type of product uom

--- addendas_walmart/models/models.py
+++ addendas_walmart/models/models.py
@@ -7,3 +7,13 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     number_order = fields.Char(related="x_studio_orden_de_compra")
+
+class AccountMove(models.Model):
+    _inherit = "account.move.line"
+
+    def get_type_product_uom(self, register):
+        for rec in register:
+            if rec.product_uom_id.name in ('Caja', 'caja', 'CAJA'):
+                return 'AE'
+            else:
+                return 'EA'
